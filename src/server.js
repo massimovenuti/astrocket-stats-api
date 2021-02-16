ca1cbb07f5e8c6e71fd68a9f3353fee611e51e97
VENUTI MASSIMO <massimo.venuti@etu.unistra.fr>
2021-02-16 10:33:15 +0100

    mise en place variables d'environnement

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const app = require('./app');
+require('dotenv').config()
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(3000);
